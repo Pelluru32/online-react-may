36bf46904e8a5d344eec7a6a64a74a5b3f43be8a
React Lab Files <95652257+reactfiles@users.noreply.github.com>
2022-05-14 22:12:23 +0530

    Add files via upload

---
+++ day3/modules/heroes.js
@@ -0,0 +1,37 @@
+/* export var ironman = {
+    firstname : "Tony",
+    lastname : "Stark"
+};
+export var spiderman = {
+    firstname : "Peter",
+    lastname : "Parker"
+};
+export var captainamerica = {
+    firstname : "Steve",
+    lastname : "Rogers"
+};
+export var hulk = {
+    firstname : "Bruce",
+    lastname : "Benner"
+};
+
+ */
+
+var ironman = {
+    firstname : "Tony",
+    lastname : "Stark"
+};
+var spiderman = {
+    firstname : "Peter",
+    lastname : "Parker"
+};
+var captainamerica = {
+    firstname : "Steve",
+    lastname : "Rogers"
+};
+var hulk = {
+    firstname : "Bruce",
+    lastname : "Benner"
+};
+export default spiderman;
+export { ironman as iman, hulk };
